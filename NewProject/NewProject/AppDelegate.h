57cccaddb609cc87a237a5661baa1cfbb5f904b9
xuzhikang <xuzhikang@xuzhikangdemacbook-pro.local>
2016-01-11 21:02:39 +0800

    新建项目

---
+++ NewProject/NewProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewProject
+//
+//  Created by xuzhikang on 16/1/11.
+//  Copyright © 2016年 xuzhikang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewProject/NewProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewProject
+//
+//  Created by xuzhikang on 16/1/11.
+//  Copyright © 2016年 xuzhikang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
